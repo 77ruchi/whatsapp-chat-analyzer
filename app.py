c601920d56e4756f0a67185bb19e7cae136aee33
77ruchi <133362211+77ruchi@users.noreply.github.com>
2026-08-25 09:41:05 +0530

    Refactor file decoding and improve plotting code

--- app.py
+++ app.py
@@ -8,17 +8,20 @@ uploaded_file = st.sidebar.file_uploader("Choose a file")
 
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
-    try:
-    data = bytes_data.decode("utf-8")
-except UnicodeDecodeError:
-    try:
-        data = bytes_data.decode("latin-1")
-    except:
-        data = bytes_data.decode("ISO-8859-1")
 
+    # ✅ FIXED DECODING
+    try:
+        data = bytes_data.decode("utf-8")
+    except UnicodeDecodeError:
+        try:
+            data = bytes_data.decode("latin-1")
+        except:
+            data = bytes_data.decode("ISO-8859-1")
+
+    # preprocess
     df = preprocessor.preprocess(data)
 
-
+    # user list
     user_list = df['user'].unique().tolist()
     if 'group_notification' in user_list:
         user_list.remove('group_notification')
@@ -28,11 +31,11 @@ except UnicodeDecodeError:
 
     selected_user = st.sidebar.selectbox("Show analysis wrt", user_list)
 
-
-    if st.sidebar.button("Show Analysis", key="analyze_btn"):
+    if st.sidebar.button("Show Analysis"):
 
         # ---------------- STATS ----------------
         num_messages, words, num_med_message, num_links = helper.fetch_stats(selected_user, df)
+
         st.title("Top statistics")
         col1, col2, col3, col4 = st.columns(4)
 
@@ -52,46 +55,48 @@ except UnicodeDecodeError:
             st.header("Links Shared")
             st.title(num_links)
 
-        #timeline
+        # ---------------- MONTHLY TIMELINE ----------------
         st.title("Monthly Timeline")
         timeline = helper.monthly_timeline(selected_user, df)
+
         fig, ax = plt.subplots()
-        plt.plot(timeline['time'], timeline['message'],color='green')
+        ax.plot(timeline['time'], timeline['message'], color='green')
         plt.xticks(rotation='vertical')
         st.pyplot(fig)
 
-
-        # daily timeline
+        # ---------------- DAILY TIMELINE ----------------
         st.title("Daily Timeline")
         daily_timeline = helper.daily_timeline(selected_user, df)
-        fig,ax = plt.subplots()
+
+        fig, ax = plt.subplots()
         ax.plot(daily_timeline['only_date'], daily_timeline['message'], color='black')
         plt.xticks(rotation='vertical')
         st.pyplot(fig)
 
-        # activity map
-        st.title('Activity Map')
+        # ---------------- ACTIVITY MAP ----------------
+        st.title("Activity Map")
         col1, col2 = st.columns(2)
-
 
         with col1:
             st.header("Most busy day")
-            busy_day = helper.week_activity_map(selected_user,df)
-            fig,ax = plt.subplots()
-            ax.bar(busy_day.index,busy_day.values,color='purple')
+            busy_day = helper.week_activity_map(selected_user, df)
+
+            fig, ax = plt.subplots()
+            ax.bar(busy_day.index, busy_day.values, color='purple')
             plt.xticks(rotation='vertical')
             st.pyplot(fig)
 
         with col2:
             st.header("Most busy month")
             busy_month = helper.month_activity_map(selected_user, df)
-            fig,ax = plt.subplots()
-            ax.bar(busy_month.index, busy_month.values,color='orange')
+
+            fig, ax = plt.subplots()
+            ax.bar(busy_month.index, busy_month.values, color='orange')
             plt.xticks(rotation='vertical')
             st.pyplot(fig)
 
+        # ---------------- HEATMAP ----------------
         st.title("Weekly Activity Map")
-
         user_heatmap = helper.activity_heatmap(selected_user, df)
 
         if user_heatmap.empty:
@@ -106,11 +111,10 @@ except UnicodeDecodeError:
             st.title("Most Busy Users")
 
             x, new_df = helper.most_busy_users(df)
-            fig, ax = plt.subplots()
-
             col1, col2 = st.columns(2)
 
             with col1:
+                fig, ax = plt.subplots()
                 ax.bar(x.index, x.values)
                 plt.xticks(rotation='vertical')
                 st.pyplot(fig)
@@ -122,11 +126,10 @@ except UnicodeDecodeError:
         st.title("Wordcloud")
 
         df_wc = helper.create_wordcloud(selected_user, df)
-        fig, ax = plt.subplots()
 
+        fig, ax = plt.subplots()
         ax.imshow(df_wc)
         ax.axis('off')
-
         st.pyplot(fig)
 
         # ---------------- MOST COMMON WORDS ----------------
@@ -137,9 +140,7 @@ except UnicodeDecodeError:
         fig, ax = plt.subplots()
         ax.bar(most_common_df['word'], most_common_df['count'])
         plt.xticks(rotation='vertical')
-
         st.pyplot(fig)
-
 
         # ---------------- EMOJI ANALYSIS ----------------
         st.title("Emoji Analysis")
@@ -152,7 +153,6 @@ except UnicodeDecodeError:
             st.dataframe(emoji_df)
 
         with col2:
-            # limit for better pie chart
             emoji_df_top = emoji_df.head(5)
 
             fig, ax = plt.subplots()
@@ -161,5 +161,4 @@ except UnicodeDecodeError:
                 labels=emoji_df_top['emoji'],
                 autopct='%1.1f%%'
             )
-
             st.pyplot(fig)
